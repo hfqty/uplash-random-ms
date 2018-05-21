5659d7fefbffb54706eaa9bc8cd506b9745b962a
zhangning <1059029968@qq.com>
2018-05-21 10:10:10 +0800

    0.6.0
    大范围重构
    添加豆瓣模块

---
+++ douban-book/src/main/java/me/ning/pro/doubanbook/DoubanBookApplication.java
@@ -0,0 +1,12 @@
+package me.ning.pro.doubanbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubanBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubanBookApplication.class, args);
+    }
+}
